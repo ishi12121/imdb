fb9689bd0a7dcdbdb8b6e1bfb3a5a94a0f2b6d57
ishi12121 <isht1402@gmail.com>
2023-05-26 23:09:45 +0530

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import Providers from "./Providers";
+export const metadata = {
+  title: 'IMDB Clone',
+  description: 'This is the IMDB clone',
+  
+}
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+      <Providers>
+      <Header />
+        {/* Header */}
+        
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+
+        {children}
+      </Providers>
+      
+      </body>
+    </html>
+  );
+}
